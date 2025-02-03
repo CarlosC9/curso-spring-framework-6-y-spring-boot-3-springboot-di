34ec22d27691fd08228aa5bd979c138228427ec1
Carlos <carlosicaraballo1999@gmail.com>
2025-02-03 09:26:00 +0000

    54. Inyectando mediante la interface

--- src/main/java/com/carlos/springboot/di/app/springboot_di/controllers/SomeController.java
+++ src/main/java/com/carlos/springboot/di/app/springboot_di/controllers/SomeController.java
@@ -1,7 +1,7 @@
 package com.carlos.springboot.di.app.springboot_di.controllers;
 
 import com.carlos.springboot.di.app.springboot_di.models.Product;
-import com.carlos.springboot.di.app.springboot_di.services.ProductServiceImpl;
+import com.carlos.springboot.di.app.springboot_di.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +15,7 @@ import java.util.List;
 public class SomeController {
 
   @Autowired
-  private ProductServiceImpl service;
+  private ProductService service;
 
   @GetMapping
   public List<Product> list() {
