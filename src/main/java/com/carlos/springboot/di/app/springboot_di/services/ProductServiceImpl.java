7c1a51ba07d60c893f18496a1109ff183c505805
Carlos <carlosicaraballo1999@gmail.com>
2025-02-03 09:37:51 +0000

    56. Inyección de dependencias vía métodos setters y constructor

--- src/main/java/com/carlos/springboot/di/app/springboot_di/services/ProductServiceImpl.java
+++ src/main/java/com/carlos/springboot/di/app/springboot_di/services/ProductServiceImpl.java
@@ -3,7 +3,6 @@ package com.carlos.springboot.di.app.springboot_di.services;
 import com.carlos.springboot.di.app.springboot_di.models.Product;
 import com.carlos.springboot.di.app.springboot_di.repositories.ProductRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,8 +11,11 @@ import java.util.stream.Collectors;
 @Service
 public class ProductServiceImpl implements ProductService {
 
-  @Autowired
   private ProductRepository repository;
+
+  public ProductServiceImpl(ProductRepository repository) {
+    this.repository = repository;
+  }
 
   @Override
   public List<Product> findAll() {
